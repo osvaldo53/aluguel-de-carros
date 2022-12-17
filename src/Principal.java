35353c80814b81229f12aab30cc00fe01dbcedeb
Osvaldo Souza <osvaldosouza53@outlook.com>
2022-12-17 11:35:41 -0300

    Aula 05/12

--- src/Principal.java
+++ src/Principal.java
@@ -1,15 +1,39 @@
+import java.util.Scanner;
+
 import menu.Menu;
+import model.Cliente;
+import service.ClienteService;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		Menu.bemVindo();
-
-		Menu.opcoesCliente();
+		boolean continua = true;
+		Scanner sc = new Scanner(System.in);
+		ClienteService clienteService = new ClienteService(sc);
 		
-		Menu.opcoesAdmin();
+		do {
+			Menu.bemVindo();
+			String cpf = sc.next();
+			if(cpf.equals("0")) {
+				break;
+			}
+			
+			Cliente clienteLogado = clienteService.buscarClientePorCpf(cpf);
+			
+			if(clienteLogado != null) {
+				System.out.println("LOGADO!!");
+			}
+			
+			Menu.opcoesCliente();
+			
+			Menu.opcoesAdmin();
+			
+			Menu.opcoesVendedor();
+		}while(continua);
 		
-		Menu.opcoesVendedor();
+		
+		
+		
 	}
 
 }
